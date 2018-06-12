8a8bf84af10825f7a862474547dc958175db50a8
Dally Wally <dallin.watac.major@gmail.com>
2018-06-11 20:07:09 -0600

    Here is where I'm at

--- assets/javascript/ttg.js
+++ assets/javascript/ttg.js
@@ -9,7 +9,7 @@ function topText4Meme() {
     var top = $(".top");
     var fontSize = parseInt(top.css("font-size"));
 
-    if (text.length > 10 && !!(text.length && !(text.length%2))) {
+    if (text.length > 10 && !!(text.length && !(text.length % 2))) {
         if (topSize < text.length) {
             fontSize--;
             top.css('font-size', fontSize.toString() + 'px');
@@ -35,7 +35,7 @@ function bottomText4Meme() {
     var bottom = $(".bottom");
     var fontSize = parseInt(bottom.css("font-size"));
 
-    if (text.length > 10 && !!(text.length && !(text.length%2))) {
+    if (text.length > 10 && !!(text.length && !(text.length % 2))) {
         if (topSize < text.length) {
             fontSize--;
             bottom.css('font-size', fontSize.toString() + 'px');
@@ -56,19 +56,46 @@ function bottomText4Meme() {
 
 $(document).ready(function () {
 
-    var memeImages = ["assets/images/wonka.jpg","assets/images/oneRing.png","assets/images/grumpycat.jpeg","assets/images/baby.png","assets/images/matrix.jpg","assets/images/cheers.jpg","assets/images/spicebae.gif","assets/images/ronaldo.jpg","assets/images/caprio.jpeg","assets/images/ron.jpg","assets/images/kick2thenutz.jpg","assets/images/getout.jpg"];
+    var memeImages = ["assets/images/wonka.jpg", "assets/images/oneRing.png", "assets/images/grumpycat.jpeg", "assets/images/baby.png", "assets/images/matrix.jpg", "assets/images/cheers.jpg", "assets/images/spicebae.gif", "assets/images/ronaldo.jpg", "assets/images/caprio.jpeg", "assets/images/ron.jpg", "assets/images/kick2thenutz.jpg", "assets/images/getout.jpg"];
 
-    $(".memeImagesHere").on("click", "img", function() {
+    $(".memeImagesHere").on("click", "img", function () {
         var image = $(this).attr("src");
 
         $('#meme4User').attr("src", image);
     })
 
-    memeImages.forEach( function(image) {
+    memeImages.forEach(function (image) {
 
-        var newImage = `<img src="${image}" alt="">`;
+        var newImage = `<img src="${image}">`;
 
         $(".memeImagesHere").append(newImage);
     })
+
+    $("#generate").on("click", function () {
+        html2canvas(document.querySelector("#meme"), {
+            allowTaint: false,
+            useCORS: false
+        }).then(canvas => {
+            console.log(canvas);
+            var img = new Image,
+                canvas = document.createElement("canvas"),
+                ctx = canvas.getContext("2d"),
+                src = "http://example.com/image"; // insert image url here
+
+            img.crossOrigin = "Anonymous";
+
+            var img = canvas.toDataURL();
+            // console.log(img);
+            // var newImage = `<img src="${img}" alt="">`;
+
+            $("#memeCreations").append(canvas);
+
+        });
+    })
+
+    // $("#Save").on("click", function() {
+    //     var newImage = $("canvas")[0].toDataURL("image/png");
+    //     console.log(newImage);
+    // })
 
 })
